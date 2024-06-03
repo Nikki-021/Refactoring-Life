09cab246d2fe98607e79ea3e7f7c65ec9291568f
nikki-021 <naime14paidican@gmail.com>
2024-06-03 20:47:28 -0300

    feature/RLA_1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    //view models
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
+    implementation ("androidx.fragment:fragment-ktx:1.3.2")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation ("androidx.activity:activity-ktx:1.2.2")
+    //picasso
+    implementation ("com.squareup.picasso:picasso:2.8")
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
+    //recyclerview
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
 }
