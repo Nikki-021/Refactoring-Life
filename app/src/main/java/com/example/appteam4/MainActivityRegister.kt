07fb8b543adff336924b2cb1358bc5f999b69945
JuanyVitaloni <juanignaciovitaloni@gmail.com>
2024-07-10 15:40:00 -0300

    Subo mi parte

--- app/src/main/java/com/example/appteam4/MainActivityRegister.kt
+++ app/src/main/java/com/example/appteam4/MainActivityRegister.kt
@@ -1,15 +1,26 @@
 package com.example.appteam4
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appteam4.databinding.ActivityMainRegisterBinding
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
+import com.example.appteam4.ui.viewmodel.RegisterEvent
+import com.example.appteam4.ui.viewmodel.ResultState
 import com.example.appteam4.ui.viewmodel.ViewModelRegister
 
 class MainActivityRegister : AppCompatActivity() {
+    private var isPasswordVisible = false
     private val viewModel by viewModels<ViewModelRegister>()
     private lateinit var binding: ActivityMainRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +33,16 @@ class MainActivityRegister : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setupTextObservers()
+        setupObservers()
+        navigation()
+        passwordTransformation()
         callRegister()
         observerRegister()
     }
 
     private fun callRegister() {
-        binding.btEnter.setOnClickListener {
+        binding.btnenter.setOnClickListener {
             viewModel.postRegister(
                 binding.etEmailRegister.text.toString(),
                 binding.etPasswordRegister.text.toString()
@@ -36,8 +51,100 @@ class MainActivityRegister : AppCompatActivity() {
     }
 
     private fun observerRegister() {
-        viewModel.data.observe(this) {
-            it.token
+        viewModel.data.observe(this){
+            when (it) {
+                is RegisterEvent.Success -> {
+                    binding.viewProgressBar.view.visibility = View.GONE
+                    binding.viewProgressBar.progressBar.visibility = View.GONE
+                    startActivity(Intent(this, HomeActivity::class.java))
+                }
+                is RegisterEvent.Loading -> {
+                    binding.viewProgressBar.view.visibility = View.VISIBLE
+                    binding.viewProgressBar.progressBar.visibility = View.VISIBLE
+                }
+                is RegisterEvent.Error -> {
+                    binding.viewProgressBar.view.visibility = View.GONE
+                    binding.viewProgressBar.progressBar.visibility = View.GONE
+                    Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+    }
+
+    private fun navigation(){
+        binding.tvLogin.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun setupTextObservers() {
+        binding.etEmailRegister.doAfterTextChanged { it: Editable? ->
+            viewModel.validateFields(
+                it.toString().trim(),
+                binding.etPasswordRegister.text.toString().trim()
+            )
+        }
+
+        binding.etPasswordRegister.doAfterTextChanged { it: Editable? ->
+            viewModel.validateFields(
+                binding.etEmailRegister.text.toString().trim(),
+                it.toString().trim()
+            )
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.isRegisterButtonEnable.observe(this) { isEnabled ->
+            binding.btnenter.isEnabled = isEnabled
+            updateRegisterButtonColor(isEnabled)
+        }
+        viewModel.registerResult.observe(this) { success ->
+            if (success) {
+                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Error en el registro", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnenter.setOnClickListener {
+            viewModel.register(
+                binding.etEmailRegister.text.toString().trim(),
+                binding.etPasswordRegister.text.toString().trim()
+            )
+        }
+    }
+    private fun updateRegisterButtonColor(isEnabled: Boolean) {
+        if (isEnabled) {
+            binding.btnenter.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.enabled_button_color
+                )
+            )
+        } else {
+            binding.btnenter.setBackgroundColor(
+                ContextCompat.getColor(
+                    this,
+                    R.color.disabled_button_color
+                )
+            )
+        }
+    }
+
+    private fun passwordTransformation() {
+        binding.etPasswordRegister.transformationMethod = PasswordTransformationMethod.getInstance()
+
+        binding.vwSquare.setOnClickListener {
+            showPassword()
+        }
+    }
+
+    private fun showPassword() {
+        if (isPasswordVisible) {
+            binding.etPasswordRegister.transformationMethod = PasswordTransformationMethod.getInstance()
+        } else {
+            binding.etPasswordRegister.transformationMethod = HideReturnsTransformationMethod.getInstance()
+        }
+        isPasswordVisible = !isPasswordVisible
     }
 }
--- app/src/main/java/com/example/appteam4/ui/viewmodel/ViewModelRegister.kt
+++ app/src/main/java/com/example/appteam4/ui/viewmodel/ViewModelRegister.kt
@@ -1,7 +1,9 @@
 package com.example.appteam4.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.appteam4.model.repository.RepositoryRegister
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -9,8 +11,14 @@ import kotlinx.coroutines.launch
 
 class ViewModelRegister(private val repositoryRegister: RepositoryRegister = RepositoryRegister()) : ViewModel() {
 
+
+
     val data = MutableLiveData<RegisterEvent>()
     private val messageError = "Error en el servicio"
+    private val _registerResult = MutableLiveData<Boolean>()
+    val registerResult: LiveData<Boolean> get() = _registerResult
+    private val _isRegisterButtonEnable = MutableLiveData<Boolean>()
+    val isRegisterButtonEnable: LiveData<Boolean> get() = _isRegisterButtonEnable
 
     fun postRegister(email: String, password: String) {
         CoroutineScope(Dispatchers.IO).launch {
@@ -22,5 +30,46 @@ class ViewModelRegister(private val repositoryRegister: RepositoryRegister = Rep
                 data.postValue(RegisterEvent.Error(messageError))
             }
         }
+    }
+    fun validateFields(email: String, password: String){
+        val emailValid = validateEmail(email)
+        val passwordValid = validatePassword(password)
+
+        _isRegisterButtonEnable.value = emailValid && passwordValid
+    }
+
+    private fun validateEmail(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        return when {
+            email.isEmpty() -> false
+            !email.matches(emailPattern.toRegex()) -> false
+            else -> true
+        }
+    }
+
+    private fun validatePassword(password: String): Boolean{
+        return when {
+            password.isEmpty() -> false
+            password.length < 8 -> false
+            !password.matches(".*[A-Z].*".toRegex()) -> false
+            !password.matches(".*[a-z].*".toRegex()) -> false
+            !password.matches(".*[0-9].*".toRegex()) -> false
+            !password.matches(".*[@#\$%^&+=].*".toRegex()) -> false
+            else -> true
+        }
+    }
+    fun register(email: String, password: String){
+        if (_isRegisterButtonEnable.value == true) {
+            viewModelScope.launch {
+                val success = simulateRegister(email, password)
+                _registerResult.value = success
+            }
+        } else {
+            _registerResult.value = false
+        }
+    }
+    private suspend fun simulateRegister(email: String,password: String): Boolean {
+        kotlinx.coroutines.delay(2000)
+        return email == "usuario@dominio.com" && password == "password123!"
     }
 }
