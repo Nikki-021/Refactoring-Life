eec63de2f893b3e1b8ee8bca8d13be51ee7c7426
nikki-021 <naime14paidican@gmail.com>
2024-07-26 04:27:08 -0300

    -Subo cambios de los campos de valicación de Register

--- app/src/main/java/com/example/appteam4/ActivityDescriptionHome.kt
+++ app/src/main/java/com/example/appteam4/ActivityDescriptionHome.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ActivityDescriptionFragment : AppCompatActivity() {
+class ActivityDescriptionHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_description_fragment)
+        setContentView(R.layout.activity_description_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
